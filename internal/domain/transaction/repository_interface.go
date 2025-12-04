eed04b82d85fa5ed507e0797cf1f54bb4c6b73d2
Attila Olbrich <olbrichattila@gmx.co.uk>
2025-12-04 23:59:15 +0000

    Rename variable

--- internal/domain/transaction/repository_interface.go
+++ internal/domain/transaction/repository_interface.go
@@ -3,5 +3,5 @@ package transaction
 type Transaction interface {
 	Create(entity TransactionEntity) error
 	List(accountNumber string) ([]TransactionEntity, error)
-	Get(transactionId string) (TransactionEntity, error)
+	Get(transactionNumber string) (TransactionEntity, error)
 }
