deedcd455da61964a168cb31d304cab0a6b5c19c
Attila Olbrich <olbrichattila@gmx.co.uk>
2025-12-07 14:01:01 +0000

    Add todo

--- internal/domain/transaction/entity_interface.go
+++ internal/domain/transaction/entity_interface.go
@@ -6,7 +6,13 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-// TODO: Add value objects here as well, instead of primitives for the getters
+// TODO: Add value objects here as well, instead of primitives for the getters for:
+// -Id
+// AccountNumber
+// UserId
+// Currency
+// Type
+
 type TransactionEntity interface {
 	Id() string
 	AccountNumber() string
